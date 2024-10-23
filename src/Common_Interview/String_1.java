14a8f3c0e2b55de57bc2c736271be2dce6eadf07
prathapvenkatesan551 <prathap2venkatesan@gmail.com>
2024-10-23 16:09:56 +0530

    CI - String 1

---
+++ src/Common_Interview/String_1.java
@@ -0,0 +1,25 @@
+package Common_Interview;
+
+public class String_1 {
+
+	public static void main(String[] args) {
+		String s="coderbyte";
+		String ct="cmhqe5b97";
+		 StringBuilder sb = new StringBuilder(s);
+	     String reversed = sb.reverse().toString();
+	     reversed+=ct;
+	     for(int i=0;i<reversed.length();i++)
+	     {
+	    	 if((i+1)%4==0)
+	    	 {
+	    		 System.out.print("_");
+	    	 }
+	    	 else
+	    	 {
+	    		 System.out.print(reversed.charAt(i));
+	    	 }
+	     }
+		
+	}
+
+}
---
+++ src/Common_Interview/String_2.java
@@ -0,0 +1,26 @@
+package Common_Interview;
+
+public class String_2 {
+
+	public static void main(String[] args) {
+		String s="233445667798";
+		
+		StringBuilder sb=new StringBuilder(s);
+		for(int i=0;i<sb.length()-1;i++)
+		{
+			int a=Character.getNumericValue(sb.charAt(i));
+			int b=Character.getNumericValue(sb.charAt(i+1));
+			if(a%2==0 && b%2==0 && a==b)
+			{
+				sb.insert(i+1, "*");
+			}
+			else if(a%2!=0 && b%2!=0 && a==b)
+			{
+				sb.insert(i+1, "-");
+			}
+			
+		}
+		System.out.println(sb);
+	}
+
+}
